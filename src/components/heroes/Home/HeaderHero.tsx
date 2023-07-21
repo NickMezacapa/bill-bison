84991bba1a24dc5a48843fdf5c9ab88629046c45
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-07-21 12:01:44 -0700

    style: update UI

--- src/components/heroes/Home/HeaderHero.tsx
+++ src/components/heroes/Home/HeaderHero.tsx
@@ -9,7 +9,7 @@ import AppDemo from '~/components/heroes/AppDemo/AppDemo'
 
 const HeaderHero = () => {
   return (
-  <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
+  <div className="overflow-hidden py-4 sm:py-32 lg:pb-32 xl:pb-36">
     <Container>
       <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
         <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-0 lg:mt-[-2rem] xl:col-span-6">
